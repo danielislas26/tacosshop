8d6317e83a420c0cc571d3d92e595f67b2fdaa6d
Daniel <danielislas22@outlook.es>
2023-09-29 14:40:19 -0600

    cambios de prueba 1

--- src/Components/Shoping.js
+++ src/Components/Shoping.js
@@ -39,7 +39,7 @@ function CartPoup() {
 
 
   const checkout = async () => {
-    await fetch('http://192.168.1.192:3001/checkout', {
+    await fetch('https://192.168.1.192:3001/checkout', {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
